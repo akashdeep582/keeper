d67153d34ad7817de554ebde38b022756c78779a
Dj <dj@Djs-MacBook-Air.local>
2021-12-14 22:48:51 +0530

    saving files

---
+++ src/components/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  function addNote(note) {
+    setNotes((prevItems) => {
+      return [...prevItems, note];
+    });
+  }
+
+  function deleteNote(id) {
+    console.log(id);
+
+    setNotes((prevItems) => {
+      return prevItems.filter((noteItems, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {console.log(notes)}
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
